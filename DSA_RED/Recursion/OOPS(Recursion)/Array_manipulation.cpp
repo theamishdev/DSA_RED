462ded4f10dbeae57bd4d34c66cce85ab78a3ec2
Amish <amishv20@gmail.com>
2026-01-26 22:24:05 +0530

    recursion for finding minimum element in an array

--- DSA_RED/Recursion/OOPS(Recursion)/Array_manipulation.cpp
+++ DSA_RED/Recursion/OOPS(Recursion)/Array_manipulation.cpp
@@ -1,28 +1,34 @@
 #include<iostream>
+#include<algorithm>
 #include <climits>
 using namespace std;
 int searching(int *arr,int size,int index,int element ){
     //base condition
     if(index==size)return -1;
+    //1 case solved
     if(arr[index] == element) {
         return index;
     }
+    //recursive call
     return searching(arr, size, index + 1, element);
-    if(arr[index]!=element)return -1;
 }
- void max_element(int *arr,int size,int index){
-    int min=INT_MIN;
+/*  void max_element(int *arr,int size,int index){  
+    } */
+void min_element(int *arr,int size,int index,int &mini){
+    //base case
     if(index==size)return;
-    if(arr[index]>min){
-        
-    }
-    max_element(arr,size,index+1);
- }
-//void min_element(){} */
+    //1 case solved
+    mini=min(mini,arr[index]);
+    //recursive call
+    min_element(arr,size,index+1,mini);
+}; 
 int main(){
 int arr[]={24,30,45,13,55,79};
 int size=6;
 int element=45;
 int index=0;
-cout<<searching(arr,size,index,element);
+int mini=INT_MAX;
+cout<<"value being searched is at: "<<searching(arr,size,index,element)<<endl;
+min_element(arr,size,index,mini);
+cout<<"Minimum Value is: "<<mini;
 }
